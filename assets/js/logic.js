58fb33f86de748a2b5b826b53a6d43aa3c6201a0
Johnathan <johnathan.mnn@gmail.com>
2022-07-20 16:54:12 -0500

    Weather API accepts user input and APIkey

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -15,17 +15,43 @@ document.addEventListener("DOMContentLoaded", function() {
   };
 });
 
+// Find current weather information
+var weatherIcon = $('#weatherIcon');
+var cityName = $('#cityName');
+var currentTemp = $('#currentTemp');
+var humidity = $('#humidity');
+var wind = $('#wind');
+var uv = $('#uv');
+
+
+// API key for the OpenWeatherMap API
+var apiKey = "711c083a0e9e1df7f231968de1c91191";
+
 // Save search history
 var list = document.getElementById('searchHistory');
 var searchBtn = $('#searchBtn');
 
 searchBtn.click(function () {
+
+    // Variables for current weather  
+    var userSearch = $("#userSearch").val();
+    var urlToday = "https://api.openweathermap.org/data/2.5/weather?q=" + userSearch + "&Appid=" + apiKey + "&units=imperial";
+    var urlForecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + userSearch + "&Appid=" + apiKey + "&units=imperial";
+
     var userSearch = document.querySelector("#userSearch").value;
     localStorage.setItem('userSearch', (userSearch));
 
     // Only adds to search history if something is actually inputted
     if(userSearch){
+        // Adds and removes classes to display the correct card
+        $('#searchPrompt').addClass('hidden');
+        $('#searchPrompt').removeClass('display');
+        $('#cityDisplay').addClass('display');
+        $('#cityDisplay').removeClass('hidden');
         addSearchHistory();
+        console.log(userSearch);
+        console.log(urlToday);
+        console.log(urlForecast);
     }
 
   // Saves search to search history
@@ -40,7 +66,9 @@ searchBtn.click(function () {
   
 });
 
+// Clears localStorage on click
 function clearData () {
     window.localStorage.clear();
     window.location.reload();
 };
+
